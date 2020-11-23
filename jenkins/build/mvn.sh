b4c6d85b5453cab302209eb64530ecf407535a65
Hutson Paul <hutson@HP-EliteBook-8530w>
2020-11-23 19:31:32 +0530

    Fixes the workspace path for execution

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,5 +3,5 @@
 echo "****************************"
 echo "****BUILDING JAR************"
 echo "****************************"
-
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/hutson/Platform/Jenkins/jenkins_data/workspace/Pipeline-Docker-Maven
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
