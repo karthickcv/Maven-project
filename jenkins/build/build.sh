b7ec99b2fa2ef6eadd8c6801ce4c83e908b6ecf7
Hutson Paul <hutson@HP-EliteBook-8530w>
2020-11-24 15:01:04 +0530

    Build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,10 +3,9 @@
 echo "**************************************"
 echo "********COPYING JAR*******************"
 echo "**************************************"
-cp -r /home/hutson/Platform/Jenkins/pipeline/java-app/target/*.jar /home/hutson/Platform/Jenkins/pipeline/jenkins/build
+cp -r /home/hutson/Platform/Jenkins/pipeline/java-app/target/*.jar $PWD
 echo "************************************************"
 echo "********BUILDING DOCKER IMAGE*******************"
 echo "************************************************"
 
-cd /home/hutson/Platform/Jenkins/pipeline/jenkins/build && docker-compose -f docker-compose-build.yml build --no-cache
-
+cd $PWD && docker-compose -f docker-compose-build.yml build --no-cache
